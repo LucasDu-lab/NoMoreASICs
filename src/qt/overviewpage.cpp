dcd2ab14c676e23612bc4493afab92d75da04efd
lucasdu-lab <ldu6509@gmail.com>
2026-05-16 12:39:05 -0400

    fix more units

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -116,7 +116,7 @@ public:
         return {DECORATION_SIZE + 8 + minimum_text_width, DECORATION_SIZE};
     }
 
-    BitcoinUnit unit{BitcoinUnit::BTC};
+    BitcoinUnit unit{BitcoinUnit::NMA};
 
 Q_SIGNALS:
     //! An intermediate signal for emitting from the `paint() const` member function.
@@ -239,7 +239,7 @@ void OverviewPage::setWalletModel(WalletModel *model)
         connect(model->getOptionsModel(), &OptionsModel::displayUnitChanged, this, &OverviewPage::updateDisplayUnit);
     }
 
-    // update the display unit, to not use the default ("BTC")
+    // update the display unit, to not use the default ("NMA")
     updateDisplayUnit();
 }
 
